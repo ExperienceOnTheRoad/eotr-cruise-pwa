bc7ed58f8bc0a73405cf803147ea8a67e0406834
Experience On The Road <info@xscapexperience.it>
2025-12-19 15:34:38 +0100

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,26 @@
+const CACHE = "eotr-cruise-v1";
+const ASSETS = ["/", "/index.html", "/adult.html", "/kids.html", "/manifest.webmanifest", "/sw.js"];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(caches.open(CACHE).then((c) => c.addAll(ASSETS)));
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((cached) => {
+      if (cached) return cached;
+      return fetch(event.request)
+        .then((res) => {
+          const copy = res.clone();
+          caches.open(CACHE).then((c) => c.put(event.request, copy));
+          return res;
+        })
+        .catch(() => caches.match("/index.html"));
+    })
+  );
+});
